674ce16acd1c4c3e150935b5d5b461bcaf978b7a
NinaFeok <ninofeoktistova@gmail.com>
2023-02-14 16:13:25 +0700

    Init

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import less from 'gulp-less';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import browser from 'browser-sync';
+
+// Styles
+
+export const styles = () => {
+  return gulp.src('src/less/style.less', { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest('src/css', { sourcemaps: '.' }))
+    .pipe(browser.stream());
+}
+
+// Server
+
+const server = (done) => {
+  browser.init({
+    server: {
+      baseDir: 'src'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch('src/less/**/*.less', gulp.series(styles));
+  gulp.watch('src/*.html').on('change', browser.reload);
+}
+
+
+export default gulp.series(
+  styles, server, watcher
+);
